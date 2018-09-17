51b033a1d868341527024888a4129a0e639cc55f
Daniel Torres <daniel.torres@liftit.co>
2018-09-17 15:36:19 -0500

    First commit

---
+++ accountservice/service/routes.go
@@ -0,0 +1,28 @@
+package service
+
+import "net/http"
+
+// Defines a single route, e.g. a human readable name, HTTP mehod and the
+// pattern the function that will execute when the route is called.
+type Route struct {
+	Name	string
+	Method	string
+	Pattern	string
+	HandlerFunc	http.HandlerFunc
+}
+
+// Defines the type Rputes which is just an array (slice) of Route structs.
+type Routes []Route
+
+// Initialize our routes
+var routes = Routes{
+	Route{
+		"GetAccount",
+		"GET",
+		"/accounts/{accountId}",
+		func(w http.ResponseWriter, r *http.Request){
+			w.Header().Set("Content-type", "application/json; charset=UTF-8")
+			w.Write([]byte("{\"result\":\"OK\"}"))
+		},
+	},
+}
---
+++ accountservice/service/webserver.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"net/http"
+	"log"
+)
+
+func StartWebServer(port string){
+	log.Println("Starting HTTP service at " + port)
+	err := http.ListenAndServe(":" + port, nil) // Goroutine will block here
+
+	if err != nil {
+		log.Println("An error ocurred starting HTTP listener at port " + port)
+		log.Println("Error: " + err.Error())
+	}
+}
